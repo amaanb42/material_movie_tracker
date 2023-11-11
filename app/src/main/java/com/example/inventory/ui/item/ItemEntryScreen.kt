77b5554207aa1d0118d0afede731d2d8606d0000
Amaan Bari <amaan@Amaans-MacBook-Pro.local>
2023-11-11 16:14:16 -0600

    delete color is now red. may need to optimize the correct shade of red

--- app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -129,7 +130,9 @@ fun ItemEntryBody(
         ItemInputForm(
             itemDetails = itemUiState.itemDetails,
             onValueChange = onItemValueChange,
-            modifier = Modifier.fillMaxWidth().background(Color.Transparent, roundedCornerShape)
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Transparent, roundedCornerShape)
         )
         Button(
             onClick = onSaveClick,
@@ -144,7 +147,8 @@ fun ItemEntryBody(
                 onClick = { deleteConfirmationRequired = true },
                 enabled = itemUiState.isEntryValid,
                 shape = roundedCornerShape,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color.Red, contentColor = Color.White)
             ) {
                 Text(text = stringResource(R.string.delete))
             }
